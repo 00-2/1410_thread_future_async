5695e5c66aa3ffdbf9fcaa93c607d89abf48f0e9
00-2 <9x02@mail.ru>
2022-04-22 14:49:16 +0300

    thread ready

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <ctime>
 #include <iomanip>
 #include <algorithm>
+#include <thread>
+
 
 #define ARR_SIZE 100
 #define n1 20
@@ -14,7 +16,38 @@ int main() {
     for (int i = 0; i< ARR_SIZE; ++i){
         arr[i] = std::rand();
     }
-    std::sort(arr.begin(), arr.begin()+arr.size()/2);
-    std::sort(arr.begin()+arr.size()/2, arr.end());
-    return 0;
+    std::thread t1([&arr] {
+        std::sort(arr.begin(), arr.begin() + arr.size() / 2);
+        for(auto el : arr){
+            std::cout << std::setprecision(3) << "THREAD1:" << el << std::endl;
+            // для задержки:
+            // std::cout << "THREAD1:";
+            // std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            // std::cout << std::setprecision(3) << el << std::endl;
+        }
+    });
+    std::thread t2([&arr] {
+        std::sort(arr.begin() + arr.size() / 2, arr.end());
+        for(auto el : arr){
+            std::cout << std::setprecision(3) << "THREAD2:" << el << std::endl;
+            // для задержки:
+            // std::cout << "THREAD2:";
+            // std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            // std::cout << std::setprecision(3) << el << std::endl;
+        }
+    });
+    std::thread t3([&arr] {
+        std::sort(arr.begin(), arr.end());
+        for(auto el : arr){
+            std::cout << std::setprecision(3) << "THREAD3:" << el << std::endl;
+            // для задержки:
+            // std::cout << "THREAD3:";
+            // std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            // std::cout << std::setprecision(3) << el << std::endl;
+        }
+    });
+    t1.join();
+    t2.join();
+    t3.join();
+    std::cout << "Завершили работу";
 }
